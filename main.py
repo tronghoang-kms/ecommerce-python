e15a58729941f6ce2f38258caab455bdb9f551b4
Trong Hoang <tronghoang@kms.com.vn>
2025-10-22 21:00:43 +0700

    user crud

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from routes import user_router
 from database.connection import init_db
 
 app = FastAPI()
@@ -10,3 +11,5 @@ async def start_db():
 @app.get("/")
 async def root():
     return {"message": "E-commerce API running successfully!"}
+
+app.include_router(user_router.router)
--- schemas/user.py
+++ schemas/user.py
@@ -1,21 +1,27 @@
-
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
+from bson import ObjectId
+from schemas.common import PyObjectId
 
 
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
     full_name: Optional[str] = None
-    password: str 
+    password: str
 
 
 class UserRead(BaseModel):
-    id: str
+    id: PyObjectId = Field(alias="_id")
     username: str
     email: EmailStr
     full_name: Optional[str] = None
-    is_active: bool
+    is_active: bool = True
+
+    class Config:
+        populate_by_name = True  
+        arbitrary_types_allowed = True  
+        json_encoders = {ObjectId: str} 
 
 
 class UserUpdate(BaseModel):
